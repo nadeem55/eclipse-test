6dd58f84253cb0b9e0a0fda0297133cbbdedbb16
ehsanwaris <ehsan.waris@yahoo.com>
2018-01-02 11:21:16 +0500

    Add a class named B.Java

---
+++ micro-service-1/src/org/nad/B.java
@@ -0,0 +1,15 @@
+package org.nad;
+
+/***
+ * B-Class
+ * @author Ehsan.Waris
+ *
+ */
+public class B {
+	public B() {
+		// TODO Auto-generated constructor stub
+		System.out.println("I'm B");
+	}
+	
+
+}
